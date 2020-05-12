f88c8a64679ff7c0d85fd754ff84de90047fd330
jfsz <843400447@qq.com>
2020-05-13 00:47:58 +0800

    20200513 后端代码提交 -- 整体机构优化，修改为maven父子项目

--- zz-admin/src/main/java/com/zz/springbootproject/module/sys/entity/SysCaptchaEntity.java
+++ zz-admin/src/main/java/com/zz/springbootproject/module/sys/entity/SysCaptchaEntity.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
-
 package com.zz.springbootproject.module.sys.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
