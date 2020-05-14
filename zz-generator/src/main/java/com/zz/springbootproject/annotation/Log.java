ef647b04ab644037b94ab6971290c429f4c309b3
jfsz <843400447@qq.com>
2020-05-15 01:12:26 +0800

    20200515 后端代码提交--mybatis-plus 配置以及代码生成器配置

--- zz-generator/src/main/java/com/zz/springbootproject/annotation/Log.java
+++
@@ -1,8 +0,0 @@
-package com.zz.springbootproject.annotation;
-/**
- * @Description: 日志注解
- * @Author: chenxue
- * @Date: 2020/5/14  16:55
- */
-public @interface Log {
-}
--- zz-generator/src/main/java/com/zz/springbootproject/aspect/LogAspect.java
+++
@@ -1,9 +0,0 @@
-package com.zz.springbootproject.aspect;
-
-/**
- * @description: 日志切面
- * @author: chenxue
- * @create: 2020-05-14 16:55
- **/
-public class LogAspect {
-}
