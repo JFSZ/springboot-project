4fcabf27822e18982e671ca2f8483057c454a974
jfsz <843400447@qq.com>
2020-05-20 18:50:10 +0800

    20200520 shiro 自定义过滤器代码提交

--- zz-admin/src/main/java/com/zz/springbootproject/module/sys/oauth2/OauthFilter.java
+++ zz-admin/src/main/java/com/zz/springbootproject/module/sys/oauth2/OauthFilter.java
@@ -1,9 +1,37 @@
 package com.zz.springbootproject.module.sys.oauth2;
 
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 /**
  * @description: shiro 过滤器
  * @author: chenxue
  * @create: 2020-05-20 15:43
  **/
-public class OauthFilter {
+public class OauthFilter extends AuthenticatingFilter {
+
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        return false;
+    }
+
+    @Override
+    protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) throws Exception {
+        return super.onLoginSuccess(token, subject, request, response);
+    }
+
+    @Override
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
+        return super.onLoginFailure(token, e, request, response);
+    }
 }
