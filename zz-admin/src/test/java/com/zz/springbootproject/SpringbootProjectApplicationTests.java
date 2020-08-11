c96e61efc3075517c5ab037d02617cafd9f975b8
jfsz <843400447@qq.com>
2020-08-11 19:16:45 +0800

    2020-08-11 代码提交

--- zz-admin/src/test/java/com/zz/springbootproject/SpringbootProjectApplicationTests.java
+++ zz-admin/src/test/java/com/zz/springbootproject/SpringbootProjectApplicationTests.java
@@ -3,11 +3,17 @@ package com.zz.springbootproject;
 import com.zz.springbootproject.module.job.entity.ScheduleJobEntity;
 import com.zz.springbootproject.module.job.utils.ScheduleUtils;
 import com.zz.springbootproject.utils.RedisUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.quartz.Scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
@@ -40,4 +46,76 @@ public class SpringbootProjectApplicationTests {
         System.out.println(hget);
     }
 
+    @Test
+    public void test2(){
+        //BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(""));
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(MyConfig.class);
+        applicationContext.refresh();
+        MyConfig myConfig = (MyConfig)applicationContext.getBean("myConfig");
+        myConfig.doWork();
+    }
+
+
+}
+
+class Fruit{
+    public Fruit(){
+        System.out.println("fruit");
+    }
+}
+
+class Apple extends Fruit{
+    public Apple(){
+        System.out.println("apple");
+    }
+}
+
+class Bananer extends Fruit{
+    public Bananer() {
+        System.out.println("bananer");
+    }
+}
+
+@Component("myConfig")
+@Slf4j
+abstract class MyConfig{
+
+    @Bean("apple")
+    @Scope("prototype")
+    public Apple getApple(){
+        return new Apple();
+    }
+    @Bean("bananer")
+    public Bananer getBananer(){
+        return new Bananer();
+    }
+    public void doWork(){
+        Fruit fruit1 = getFruit1();
+        Fruit fruit11 = getFruit11();
+        Fruit fruit2 = getFruit2();
+        Fruit fruit21 = getFruit21();
+        log.info("Fruit1's hashcode is : {}",fruit1.hashCode());
+        log.info("Fruit1's hashcode is : {}",fruit11.hashCode());
+        log.info("Fruit2's hashcode is : {}",fruit2.hashCode());
+        log.info("Fruit2's hashcode is : {}",fruit21.hashCode());
+
+    }
+    @Lookup("apple")
+    protected abstract Fruit getFruit1();
+
+    @Lookup("apple")
+    protected abstract Fruit getFruit11();
+
+    @Lookup("bananer")
+    protected abstract Fruit getFruit2();
+    @Lookup("bananer")
+    protected abstract Fruit getFruit21();
+
+}
+
+@Component
+@Scope("prototype")
+abstract class FruitPlate{
+     protected abstract Fruit getFruit();
 }
